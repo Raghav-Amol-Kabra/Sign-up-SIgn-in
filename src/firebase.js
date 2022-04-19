5a13c31af28d7e8e838bbe4210cdd966d7a031bc
Raghav-Amol-Kabra <104000772+Raghav-Amol-Kabra@users.noreply.github.com>
2022-04-19 19:58:08 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,13 @@
-import firebase from "firebase/app"
-import "firebase/auth"
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyBgyLxAcCvS6zFdwA96KfNCjCrkMQcu1DI",
+    authDomain: "w4stores-auth.firebaseapp.com",
+    projectId: "w4stores-auth",
+    storageBucket: "w4stores-auth.appspot.com",
+    messagingSenderId: "427202011168",
+    appId: "1:427202011168:web:e4e0771a00f4e262e55c45"
 })
-
 export const auth = app.auth()
 export default app
